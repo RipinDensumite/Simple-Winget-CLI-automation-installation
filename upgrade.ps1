2abc35048fb3b7f8b33cc10403b0a228ada76eda
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2024-04-28 16:21:27 +0800

    Update upgrade.ps1

--- upgrade.ps1
+++ upgrade.ps1
@@ -1,1 +1,12 @@
+# Check if winget is installed. If not, provide instructions to manually install.
+if (!(Get-Command winget -ErrorAction SilentlyContinue)) {
+    Write-Host "winget not found. Please install winget manually. See https://aka.ms/getwinget"
+    Exit
+}
 
+# Upgrade package
+winget upgrade --all --accept-package-agreements --accept-source-agreements
+
+Write-Host ""
+Write-Host "Upgrade complete! Press any key to continue..."
+Read-Host 
